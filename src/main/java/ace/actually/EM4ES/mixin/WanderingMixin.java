830617490e70cc93e8fc1c410926b3c96136b45a
C10udburst <18114966+C10udburst@users.noreply.github.com>
2021-12-31 16:32:56 +0100

    improvements

--- src/main/java/ace/actually/EM4ES/mixin/WanderingMixin.java
+++ src/main/java/ace/actually/EM4ES/mixin/WanderingMixin.java
@@ -3,16 +3,13 @@ package ace.actually.EM4ES.mixin;
 import ace.actually.EM4ES.EM4ES;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.MerchantEntity;
-import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,25 +17,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WanderingTraderEntity.class)
 public abstract class WanderingMixin extends MerchantEntity {
 
-
-
     public WanderingMixin(EntityType<? extends MerchantEntity> entityType, World world) {
         super(entityType, world);
     }
 
     @Inject(at = @At("TAIL"), method = "fillRecipes")
-    public void addTrades(CallbackInfo ci)
-    {
-
-        if(!world.isClient)
-        {
+    public void addTrades(CallbackInfo ci) {
+        if(!world.isClient) {
             ServerWorld world = (ServerWorld) getWorld();
-            for (int i = 1; i < 6; i++) {
-                offers.add(new TradeOffer(new ItemStack(Items.EMERALD, random.nextInt(64)), EM4ES.makeRandomMap(world, getBlockPos()), 1, 1, 1));
-
-            }
+            Thread thread = new Thread(() -> {
+                for (int i = 0; i < random.nextInt(1,8); i++) {
+                    offers.add(new TradeOffer(new ItemStack(Items.EMERALD, random.nextInt(1,64)), EM4ES.makeRandomMap(world, getBlockPos()), 1, 1, 1));
+                }
+            });
+            thread.start();
         }
     }
-
-
 }
